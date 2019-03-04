f9207b709833f26e2223aa43ee91bb05663106a9
Keston <kestonc@cca.edu>
2019-03-04 08:36:38 -0800

    bob

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('#container').toggleClass('colorful');  
+  });
+});
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('h1').toggleClass('colorful2');  
+  });
+});
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('h3').toggleClass('colorful1');  
+  });
+});
+
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('a').toggleClass('colorful1');  
+  });
+});
+
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('#header').toggleClass('colorful3');  
+  });
+});
+
+$('document').ready(function(){
+  $('body').click(function(){
+    $('p').toggleClass('colorful2');  
+  });
+});
