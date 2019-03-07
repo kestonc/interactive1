4adf15c273d584ac75e75657cf4c6eef673066f3
Keston <kestonc@cca.edu>
2019-03-07 11:27:58 -0800

    bob

---
+++ sensory/js/script.js
@@ -0,0 +1,23 @@
+$('document').ready(function(){
+  $('p').click(function(){
+    $('div').addClass('animation1');  
+  });
+});
+
+$('document').ready(function(){
+  $('p').click(function(){
+    $('#bob').addClass('animation2');  
+  });
+});
+
+$('document').ready(function(){
+  $('p').click(function(){
+    $('p').addClass('goodbye');  
+  });
+});
+
+$('document').ready(function(){
+  $('p').click(function(){
+    $('h1').addClass('animation3');  
+  });
+});
